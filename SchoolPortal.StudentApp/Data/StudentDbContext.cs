d08f465eb764c9f1a68be669a739c679d95eaa84
ibrahimzaher <hemazaher15@gmail.com>
2026-05-22 16:42:54 +0300

    Add EF Core setup and initial migration for Students
    Configured EF Core with SQL Server in `Program.cs` and added `StudentDbContext` for database context. Updated `appsettings.json` with connection string. Created `Student` model and `StudentConfiguration` for entity mapping. Generated initial migration to create `Students` table and added migration snapshot.

---
+++ SchoolPortal.StudentApp/Data/StudentDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SchoolPortal.StudentApp.Models;
+
+namespace SchoolPortal.StudentApp.Data;
+
+public class StudentDbContext:DbContext
+{
+    public StudentDbContext(DbContextOptions<StudentDbContext> options) : base(options)
+    {
+    }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(StudentDbContext).Assembly);
+    }
+    public DbSet<Student> Students { get; set; } = null!;
+}
---
+++ SchoolPortal.StudentApp/Models/Student.cs
@@ -0,0 +1,12 @@
+﻿namespace SchoolPortal.StudentApp.Models;
+
+public class Student
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; } = null!;
+    public string LastName { get; set; } = null!;
+    public string Email { get; set; } = null!;
+    public DateTime? DateOfBirth { get; set; }
+    public DateTime EnrollmentDate { get; set; } = DateTime.UtcNow;
+
+}
