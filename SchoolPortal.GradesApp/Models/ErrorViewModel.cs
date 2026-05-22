2556a4bd5ce28907956ce320952cb193bd9f2415
ibrahimzaher <hemazaher15@gmail.com>
2026-05-22 20:59:53 +0300

    Init GradesApp and install Dependencies

---
+++ SchoolPortal.GradesApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SchoolPortal.GradesApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
